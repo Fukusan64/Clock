8a3635a204ed8f001b33ba57c6d119d4083d33bf
Fukusan64 <ikishikawa64@gmail.com>
2019-10-23 09:27:53 +0900

    テンプレートの関数の定義順を直した

--- template/js/main.js
+++ template/js/main.js
@@ -9,16 +9,16 @@ window.onload = () => {
   }
 
   //# 画面更新のときに使う関数を作る
-  // 中心点,角度,半径から座標を計算する関数
-  function getPos() {
-  }
-
   //文字盤を描画する関数
   function drawFace() {
   }
 
   // 針を描画する関数
   function drawHand() {
+  }
+
+  // 中心点,角度,半径から座標を計算する関数
+  function getPos() {
   }
 
   // 起動
